5e1a80b508bc3d32b8a7d8203a3d87075434b057
Ken Sodemann <ken@ionic.io>
2019-10-08 17:22:09 -0500

    do tab positions per platform

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  isIOS: boolean;
 
-  constructor() {}
-
+  constructor(platform: Platform) {
+    this.isIOS = platform.is('ios');
+  }
 }
